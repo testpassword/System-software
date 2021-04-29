bfb157eae6279e0855c78b788a14ab7fa4213c29
progML <matveylesonen@gmail.com>
2021-04-29 04:39:37 +0300

    add partitions.c

--- headers/main.h
+++ headers/main.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#define SYS_BLOCK_DIR "/sys/block/" //https://www.kernel.org/doc/html/latest/block/stat.html
-#define DRIVE_PATTERN "^((sd|hd)[a-z])|(nvme[0-9])"
-
 int partitionsMode();
 
 int shellMode(char *name);
--- sources/main.c
+++ sources/main.c
@@ -2,7 +2,6 @@
 #include "../headers/hfsplus_utils.h"
 
 
-
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
@@ -14,7 +13,9 @@
 int main(int argc, char * argv[]) {
 
     if (argc == 2 && strcmp(argv[1], "partitions") == 0) {
-        partitionsMode();
+          partitionsMode();
+//        print_devices(result);
+//        deviceMode();
         return 0;
     }
     if (argc == 3 && strcmp(argv[1], "shell") == 0) {
@@ -28,44 +29,44 @@ int main(int argc, char * argv[]) {
 }
 
 //Вывод списка дисков и разделов подключенных к ОС.
-int partitionsMode() {
-    DIR *block_devices_dir;
-    DIR *partitions_dir;
-    struct dirent *block_device_entry;
-    struct dirent *partition_entry;
-    regex_t matcher_drives;
-    regex_t matcher_partitions;
-
-
-    regcomp(&matcher_drives, DRIVE_PATTERN,
-            REG_EXTENDED); //https://www.opennet.ru/cgi-bin/opennet/man.cgi?topic=regcomp&category=3
-    block_devices_dir = opendir(SYS_BLOCK_DIR);
-    if (!block_devices_dir) {
-        return -1;
-    }
-    while ((block_device_entry = readdir(block_devices_dir)) != NULL) {
-        if (!regexec(&matcher_drives, block_device_entry->d_name, 0, NULL, 0)) {
-            printf("Drive %s\n", block_device_entry->d_name);
-
-            regcomp(&matcher_partitions, block_device_entry->d_name, 0);
-            char drive_dir[strlen(SYS_BLOCK_DIR) + strlen(block_device_entry->d_name) + 1];
-            drive_dir[0] = 0;
-            strcat(drive_dir, SYS_BLOCK_DIR);
-            strcat(drive_dir, block_device_entry->d_name);
-            partitions_dir = opendir(drive_dir);
-            if (!partitions_dir) {
-                return -1;
-            }
-            while ((partition_entry = readdir(partitions_dir)) != NULL) {
-                if (!regexec(&matcher_partitions, partition_entry->d_name, 0, NULL, 0)) {
-                    printf("\t- partition %s\n", partition_entry->d_name);
-                }
-            }
-        }
-    }
-    closedir(block_devices_dir);
-    return 0;
-}
+//int deviceMode() {
+//    DIR *block_devices_dir;
+//    DIR *partitions_dir;
+//    struct dirent *block_device_entry;
+//    struct dirent *partition_entry;
+//    regex_t matcher_drives;
+//    regex_t matcher_partitions;
+//
+//
+//    regcomp(&matcher_drives, DRIVE_PATTERN,
+//            REG_EXTENDED); //https://www.opennet.ru/cgi-bin/opennet/man.cgi?topic=regcomp&category=3
+//    block_devices_dir = opendir(SYS_BLOCK_DIR);
+//    if (!block_devices_dir) {
+//        return -1;
+//    }
+//    while ((block_device_entry = readdir(block_devices_dir)) != NULL) {
+//        if (!regexec(&matcher_drives, block_device_entry->d_name, 0, NULL, 0)) {
+//            printf("Drive %s\n", block_device_entry->d_name);
+//
+//            regcomp(&matcher_partitions, block_device_entry->d_name, 0);
+//            char drive_dir[strlen(SYS_BLOCK_DIR) + strlen(block_device_entry->d_name) + 1];
+//            drive_dir[0] = 0;
+//            strcat(drive_dir, SYS_BLOCK_DIR);
+//            strcat(drive_dir, block_device_entry->d_name);
+//            partitions_dir = opendir(drive_dir);
+//            if (!partitions_dir) {
+//                return -1;
+//            }
+//            while ((partition_entry = readdir(partitions_dir)) != NULL) {
+//                if (!regexec(&matcher_partitions, partition_entry->d_name, 0, NULL, 0)) {
+//                    printf("\t- partition %s\n", partition_entry->d_name);
+//                }
+//            }
+//        }
+//    }
+//    closedir(block_devices_dir);
+//    return 0;
+//}
 
 int shellMode(char *filename) {
     FileSystem *fileSystem = openFileSystem(filename);
